d921364a9d72bacac9d42af86c6d2c6d1e76602c
Nathan Wilson <nswilson@gmail.com>
2017-09-26 22:52:42 -0400

    Create nouislider from given options

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import nouislider from 'nouislider';
+// import '../node_modules/nouislider/distribute/nouislider.css';
+
+function Slider(props) {
+  return (
+    <div ref={props.slider} />
+  );
+}
+
 class NoUiSlider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: this.props.options.start,
+    };
+  }
+
+  componentDidMount() {
+    nouislider.create(this.sliderElement, this.props.options);
+    /* nouislider.create(this.sliderElement, {
+      start: this.state.values,
+      connect: true,
+      range: {
+        min: 0,
+        max: 100,
+      },
+    }); */
+  }
+
   render() {
     return (
-      <div>This is extra Fancy!</div>
+      <div className="bar">
+        <Slider slider={el => this.sliderElement = el} />
+        <p>{this.state.values.toString()}</p>
+      </div>
     );
   }
 }
+
 export default NoUiSlider;
